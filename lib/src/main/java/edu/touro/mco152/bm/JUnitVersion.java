f7b8a74c0d374ba66baab2b0fb3be6c282f43931
Duskid123 <duskisdovid@gmail.com>
2024-04-19 14:46:43 -0400

    changed the junitversion to set the properties when setting the callable.

--- lib/src/main/java/edu/touro/mco152/bm/JUnitVersion.java
+++ lib/src/main/java/edu/touro/mco152/bm/JUnitVersion.java
@@ -23,6 +23,7 @@ public class JUnitVersion implements UIHandler {
 
     @Override
     public void setCallable(Callable callable) {
+        setProperties();
         this.callable = callable;
     }
 
@@ -74,7 +75,6 @@ public class JUnitVersion implements UIHandler {
 
     @Override
     public void start() {
-        setProperties();
         futureTask = new FutureTask<>(callable);
         Thread thread = new Thread(futureTask);
         thread.start();
